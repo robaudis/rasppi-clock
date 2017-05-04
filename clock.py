1c04c94bc90086b88e46cee30dd5ff50f5abceb3
Rob Audis <robaudis@gmail.com>
2017-05-04 19:54:56 +0100

    Adding a new class to simplify the main loop for scrolling the text

--- clock.py
+++ clock.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 from sighandler import SigHandler
 from weatherfetcher import WeatherFetcher
+from contextlib import contextmanager
 import time
 import os
 import argparse
@@ -22,21 +23,46 @@ def make_font(fontname, size):
     scriptpath = os.path.dirname(os.path.abspath(__file__))
     return ImageFont.truetype(os.path.join(scriptpath, 'fonts', '%s.ttf' % fontname), size)
 
+class TextScroller:
+    def __init__(self, text, screenwidth = 20):
+        self.text = text
+        self.scrolling = False
+        self.slicepos = 0
+        self.waitcount = 0
+        self.extrachars = len(text) - screenwidth if len(text) > screenwidth else 0
+
+    @contextmanager
+    def scroll(self, scrolldelay = 3, overscroll = 0):
+        if self.slicepos > self.extrachars + overscroll:
+            self.slicepos = 0
+            self.waitcount = 0
+            self.scrolling = False
+
+        yield self.text[self.slicepos:]
+
+        self.waitcount += 1
+        if self.extrachars and self.waitcount > scrolldelay:
+            self.scrolling = True
+            self.slicepos += 1
+
 class ForecastPicker:
     def __init__(self, weatherfetcher):
         self.requests = 0
         self.forecastnum = 0
         self.fetcher = weatherfetcher
+        self.default = TextScroller('No forecast data')
+        self.current = None
     
     def forecast(self):
         if len(self.fetcher.forecasts):
             self.requests += 1
-            if self.requests > 20:
+            if self.current is None or (self.requests > 20 and not self.current.scrolling):
                 self.requests = 0
                 self.forecastnum = self.forecastnum + 1 if self.forecastnum < 2 else 0
+                self.current = TextScroller(self.fetcher.forecasts[self.forecastnum])
             
-            return self.fetcher.forecasts[self.forecastnum]
-        return 'No forecast data'
+            return self.current
+        return self.default
 
 parser = argparse.ArgumentParser(description='OLED clock with weather.')
 parser.add_argument('--apikey', help='Weather Underground API key.')
@@ -57,8 +83,6 @@ end = datetime.strptime('22:30', '%H:%M').time()
 
 sighandler = SigHandler()
 
-slicepos = 0
-extrachars = 0
 with WeatherFetcher(args.apikey, 300) as weather:
     forecastpicker = ForecastPicker(weather)
     while not sighandler.kill_now:
@@ -70,21 +94,14 @@ with WeatherFetcher(args.apikey, 300) as weather:
             with canvas(device) as draw:
                 format = '%H:%M' if not space else '%H %M'
                 draw.text((2,2), datetime.now().strftime(format), font=clockfont, fill='white')            
-                draw.text((5,40), weather.current, font=infofont, fill='white')
-                #draw.text((5,50), datetime.now().strftime('%d-%m-%Y'), font=infofont, fill='white')
-                forecast = forecastpicker.forecast()
-                length = len(forecast)
-                
-                extrachars = length - 20 if length - 20 else zero
-                draw.text((5,50), forecast[slicepos:], font=infofont, fill='white')
+                draw.text((5,40), weather.current, font=infofont, fill='white')  
+                with forecastpicker.forecast().scroll(overscroll=3) as text:              
+                    draw.text((5,50), text, font=infofont, fill='white')
         else:
             if screen_on:
                 device.hide()
                 screen_on = False
         
-        slicepos += 1
-        if slicepos > extrachars:
-            slicepos = 0
         space = not space
         time.sleep(0.5)
 
